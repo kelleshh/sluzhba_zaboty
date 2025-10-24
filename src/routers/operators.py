e7e9c2244fedd1b46c438981c42b3ffa12c3a1b0
kelleszaw <glikosd@yahoo.com>
2025-10-24 20:37:03 +0300

    feat(routers): implement operator ticket management logic
    1) integrate database access via SessionLocal
    2) add claim handler assigning operator to waiting ticket
    3) add finish handler closing dialogue and notifying user
    4) include user/operator messaging and proper status updates

--- src/routers/operators.py
+++ src/routers/operators.py
@@ -1,5 +1,10 @@
 from aiogram import Router, F, types
 from aiogram.types import CallbackQuery
+from src.db.base import SessionLocal
+from src.db.models import Ticket, TicketStatus, User
+from src.keyboards.operator import finish_kb
+from src.texts import OP_CONNECTED, OP_DISCONNECTED
+from datetime import datetime
 
 router = Router()
 
@@ -8,7 +13,39 @@ async def claim_ticket(c: CallbackQuery):
     ticket_id = int(c.data.split(':')[1])
     operator_id = c.from_user.id
 
-    #with SessionLocal
+    with SessionLocal() as s:
+        t = s.get(Ticket, ticket_id)
+        if not t or t.status != TicketStatus.waiting:
+            await c.answer('Уже занято или неактуально', show_alert=True)
+            return
+        t.status = TicketStatus.assigned
+        t.operator_tg_id = operator_id
+        s.commit()
+        u = s.get(User, t.user_id)
+    
+    # сообщение оператору
+    msg = f'Вы взяли тикет #{ticket_id} (пользователь {u.first_name}, айди @{u.username}. \n Ведите переписку тут - бот все перекинет пользователю.)' # type: ignore
+    await c.bot.send_message(operator_id, msg, reply_markup=finish_kb(ticket_id)) # type: ignore
+    await c.answer('Тикет закреплен за вами')
+    await c.bot.send_message(u.tg_id, OP_CONNECTED) #type: ignore
 
-    # TODO: допилить датабазу сначала
-    pass
+
+    @router.callback_query(F.data.startswith('finish:'))
+    async def finis_ticket(c: CallbackQuery):
+        ticket_id = int(c.data.split(':')[1])
+        operator_id = c.from_user.id
+
+        with SessionLocal() as s:
+            t = s.get(Ticket, ticket_id)
+            if not t or t.operator_tg_id != operator_id:
+                await c.answer('Это не ваш диалог', show_alert=True)
+                return
+            t.status = TicketStatus.closed
+            t.closed_at = datetime.now()
+            s.commit()
+            user_tg = t.user.tg_id
+
+        await c.bot.send_message(user_tg, OP_DISCONNECTED) #type: ignore
+        await c.message.edit_text('Диалог закрыт.') # type:ignore
+        await c.answer()
+        
