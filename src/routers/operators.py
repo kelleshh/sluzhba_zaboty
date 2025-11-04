7d5477f556c62814111ec211b4d459641aa310b1
kelleshh <shabieieie@gmail.com>
2025-11-04 21:51:55 +0300

    feat(operators): "История" replaced by "Содержание" in starting message

--- src/routers/operators.py
+++ src/routers/operators.py
@@ -74,7 +74,7 @@ async def claim_ticket(c: CallbackQuery):
 
     # сразу дублируем историю заявки в ЛС оператора
     if user_msgs:
-        await c.bot.send_message(operator_id, f"История заявки #{ticket_id}:")  # type: ignore
+        await c.bot.send_message(operator_id, f"Содержание заявки #{ticket_id}:")  # type: ignore
         for tm in user_msgs:
             try:
                 # метка "кто и что"
